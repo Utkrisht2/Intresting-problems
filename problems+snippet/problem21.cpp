8b9b93f34c497e68996a24e8480472da0372e3e9
Utkrisht Kumar singh <135011150+Utkrisht2@users.noreply.github.com>
2023-11-11 21:54:26 +0530

    Day21

---
+++ problems+snippet/problem21.cpp
@@ -0,0 +1,20 @@
+/*Chef's professor is planning to give his class a group assignment. There are 2N students in the class, with distinct roll numbers ranging from 
+1 to 2N. Chef's roll number is X.
+The professor decided to create N groups of 2 students each. The groups were created as follows: the first group consists of roll numbers 
+1 and 2N, the second group of roll numbers 2 and 2N−1, and so on, with the final group consisting of roll numbers N+1.
+Chef wonders who his partner will be. Can you help Chef by telling him the roll number of his partner?*/#include <iostream>
+
+#include<iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n, x;
+	    cin>>n>>x;
+	    cout<<2*n-(x-1)<<endl;
+	}
+	return 0;
+}
