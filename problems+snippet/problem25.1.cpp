7a7d55d70521d36a00037ee38dfd1464b7008215
Utkrisht Kumar singh <135011150+Utkrisht2@users.noreply.github.com>
2023-11-15 23:11:03 +0530

    Day25

---
+++ problems+snippet/problem25.1.cpp
@@ -0,0 +1,25 @@
+/*A palindromic prime number is a positive integer that is both a prime number (meaning it has exactly two divisors: 1 and itself) and a palindrome in base 
+10 (meaning it reads the same both backwards and forwards).Leading zeros are not considered when determining if a number is a palindrome (so 
+20 is not a palindrome, even though it can be written as 020).
+You are given an integer N.Consider the first N palindromic prime numbers.
+ How many of them have an even number of digits, and how many of them have an odd number of digits?*/
+
+ #include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n<=4){
+	        cout<<0<<" "<<n<<endl;
+	    }
+	    else{
+	        cout<<1<<" "<<n-1<<endl;
+	    }
+	}
+	return 0;
+}
