805600fd4153153e2290827ac4cf8b646c424bc4
Utkrisht Kumar singh <135011150+Utkrisht2@users.noreply.github.com>
2024-01-17 11:34:35 +0530

    Day86

---
+++ problems+snippet/problem86.cpp
@@ -0,0 +1,17 @@
+/*Given an integer N, determine the number of pairs (A,B) such that: 1≤A,B≤N; A+B is odd.*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        long long n;
+        cin>>n;
+        
+        cout<<((n*n)/2)<<endl;
+    }
+    return 0;
+}
